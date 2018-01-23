93feabaaec37f1066d9b6bf7c13dd33bf4f890e7
codegrabber1 <poruchenko@ukr.net>
2018-01-23 10:14:24 +0200

    1014_2301

--- js/custom.js
+++ js/custom.js
@@ -69,10 +69,29 @@ jQuery(document).ready(function($){
         }, 800); return false;
     });
 // headerBigSlider
-//     $("#topSlider").owlCarousel({
-//         items:1
-//
-//     });
+    var $item = $('.carousel .item');
+    var $wHeight = $(window).height();
+    $item.eq(0).addClass('active');
+    $item.height($wHeight);
+    $item.addClass('full-screen');
+
+    $('.carousel img').each(function() {
+        var $src = $(this).attr('src');
+        var $color = $(this).attr('data-color');
+        $(this).parent().css({
+            'background-image' : 'url(' + $src + ')',
+            'background-color' : $color
+        });
+        $(this).remove();
+    });
+
+    $(window).on('resize', function (){
+        $wHeight = $(window).height();
+        $item.height($wHeight);
+    });
+    $('.carousel').carousel({
+        interval: 5000 //changes the speed
+    });
 // owlCarousel
     $("#owl-propose").owlCarousel({
         pagination: true,
